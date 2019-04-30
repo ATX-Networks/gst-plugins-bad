aedb1d80a76706fd7169220759a1c45453534cb7
Mathieu Duponchelle <mathieu@centricular.com>
2019-05-01 01:09:19 +0200

    mpegtsmux: aggregator port

--- gst/mpegtsmux/tsmux/tsmux.c
+++ gst/mpegtsmux/tsmux/tsmux.c
@@ -1078,7 +1078,7 @@ pad_stream (TsMux * mux, TsMuxStream * stream, gint64 cur_ts)
         GST_LOG ("Transport stream bitrate: %" G_GUINT64_FORMAT, bitrate);
 
         if (bitrate < mux->bitrate) {
-          GST_LOG_OBJECT (mux, "Padding transport stream");
+          GST_LOG ("Padding transport stream");
 
           if (!tsmux_get_buffer (mux, &buf)) {
             ret = FALSE;
